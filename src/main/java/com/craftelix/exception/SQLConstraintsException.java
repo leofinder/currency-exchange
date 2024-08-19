c41915b1096fce6e17bf4ee4a49f3f2247e203c9
leofinder <imrhyde@yandex.ru>
2024-08-19 16:35:50 +0500

    feat: add exception classes

---
+++ src/main/java/com/craftelix/exception/SQLConstraintsException.java
@@ -0,0 +1,8 @@
+package com.craftelix.exception;
+
+public class SQLConstraintsException extends RuntimeException {
+
+    public SQLConstraintsException(String message) {
+        super(message);
+    }
+}
